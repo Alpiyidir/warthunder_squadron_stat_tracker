88a82f561f411eef03da955df6a9f3d4e3d39276
Alpiyidir <solakoglualp@yahoo.com>
2022-07-14 01:01:40 +0300

    good version with working update for squadron, before changing update to go all at once

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ class SquadronInfoTracker:
             if rowCounter == 1:
                 playerKey = row.text.lower()
             elif rowCounter == 2:
-                playerRatings[playerKey] = row.text
+                playerRatings[playerKey] = int(row.text)
 
             rowCounter += 1
             if rowCounter == 6:
@@ -72,8 +72,6 @@ class SquadronInfoTracker:
             return -1
 
     def update_squadron_info(self, squadronName):
-        self.go_to_squadron_page(squadronName)
-
         playerRatings = self.get_players_ratings_from_squadron(squadronName)
 
         conn = sqlite3.connect("squadronstats.db")
@@ -85,43 +83,96 @@ class SquadronInfoTracker:
         squadron = c.fetchone()
 
         if squadron:
-            squadronId = squadron["id"]
+            currentSquadronId = squadron["id"]
         # Else, this is the first instance of this squadron being updated so the squadron name is added to the sq. table
         else:
             c.execute("INSERT INTO squadrons (name) VALUES (?)", [squadronName])
+            conn.commit()
 
+            c.execute("SELECT id FROM squadrons WHERE name = ?", [squadronName])
+            currentSquadronId = c.fetchone()["id"]
 
-        for name, currentRating in playerRatings.items():
-            c.execute("SELECT id FROM players WHERE name = ?", [name])
+        for playerName, currentRating in playerRatings.items():
+            c.execute("SELECT id FROM players WHERE name = ?", [playerName])
             player = c.fetchone()
+
+            # If player is not already in the database, creates a new entry for them and gets their id, otherwise uses
+            # existing id
             if player:
                 playerId = player["id"]
+            else:
+                c.execute("INSERT INTO players (name) VALUES (?)", [playerName])
+                conn.commit()
+                c.execute("SELECT id FROM players WHERE name = ?", [playerName])
+                playerId = c.fetchone()["id"]
 
-                c.execute("SELECT rating FROM actions WHERE player_id = ?", [playerId])
-                dbRating = c.fetchone()["rating"]
+            # If player is already in the database checks if the last rating entry has the same rating as the current
+            # rating entry, otherwise, it just writes the current rating entry as the player has never been logged
+            # before
+            currentTime = int(time.time())
+            dbUpdatedForPlayer = False
+            # Fetches most recent rating entry using timestamp
+            c.execute(
+                "SELECT rating, squadron_id, timestamp FROM activity WHERE player_id = ? ORDER BY timestamp DESC LIMIT 1",
+                [playerId])
+            playerInfo = c.fetchone()
+            if playerInfo:
+                dbRating = playerInfo["rating"]
+                dbSquadronId = playerInfo["squadron_id"]
+                dbTimestamp = playerInfo["timestamp"]
 
                 # If the current rating of the player is the same as the database entry is the same, no new entry is
                 # created and the timestamp for the db entry is edited, otherwise, a new entry with the new timestamp of
                 # the time this rating was first seen is created
-                if currentRating == dbRating:
-                    c.execute("UPDATE actions SET timestamp = ? WHERE player_id = ?", [time.time(), playerId])
-                else:
-                    c.execute("INSERT INTO actions (player_id, squadron_id, rating, timestamp) VALUES (?, ?, ?, ?)",
-                              [playerId, squadronId, currentRating, time.time()])
-            else:
+                if currentRating == dbRating and currentSquadronId == dbSquadronId:
+                    c.execute("UPDATE activity SET timestamp = ? WHERE player_id = ? AND timestamp = ?",
+                              [currentTime, playerId, dbTimestamp])
+                    conn.commit()
+                    dbUpdatedForPlayer = True
+
+            # If no action has yet been done on the db, there either wasn't a player that existed, or if there was a
+            # player that previously existed in the db their rating/squadron has changed therefore a new entry is made
+            if not dbUpdatedForPlayer:
+                c.execute("INSERT INTO activity (player_id, squadron_id, rating, timestamp) VALUES (?, ?, ?, ?)",
+                          [playerId, currentSquadronId, currentRating, currentTime])
+                conn.commit()
 
         conn.close()
 
-    def update_info_for_all_squadrons(self):
-        print("")
+    def scrape(self):
+        conn = sqlite3.connect("squadronstats.db")
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
 
+        c.execute("SELECT name FROM squadrons")
+        squadrons = c.fetchall()
+        for squadron in squadrons:
+            self.go_to_squadron_page(squadron["name"])
+            print(squadron["name"])
+
+    # TODO check for auto redirects
+    def update_info_for_all_squadrons(self):
+        conn = sqlite3.connect("squadronstats.db")
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+
+        c.execute("SELECT name FROM squadrons")
+        squadrons = c.fetchall()
+        for squadron in squadrons:
+            print(squadron["name"])
+            self.update_squadron_info(squadron["name"])
+
+    # TODO don't forget if a player changes squadrons during the player search they will have to get added to the list
+    # TODO again, if this happens nuke their old player entry in the table until their new squadron is added
     @staticmethod
     def get_player_rating_from_db(playerName):
         conn = sqlite3.connect("squadronstats.db")
         conn.row_factory = sqlite3.Row
         c = conn.cursor()
 
-        c.execute("SELECT rating FROM activity WHERE player_id IN(SELECT id FROM players WHERE name = ?)", [playerName])
+        c.execute(
+            "SELECT rating, squadron_info FROM activity WHERE player_id IN(SELECT id FROM players WHERE name = ?) ORDER BY timestamp DESC",
+            [playerName])
         row = c.fetchone()
 
         if not row:
@@ -133,6 +184,6 @@ class SquadronInfoTracker:
 
 
 a = SquadronInfoTracker()
-print(a.get_player_rating_from_squadron("Immortal Legion", "Alpiyidir"))
-#print(a.get_player_rating_from_db("Alpiyidir"))
-a.update_squadron_info("Immortal Legion")
+# print(a.get_player_rating_from_squadron("Immortal Legion", "Alpiyidir"))
+# print(a.get_player_rating_from_db("Alpiyidir"))
+a.update_info_for_all_squadrons()
